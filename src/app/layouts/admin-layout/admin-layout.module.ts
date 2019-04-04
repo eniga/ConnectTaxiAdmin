e4f27c66309ef8ddb8b03df20a5eaa6ec04be1f5
Eniga Ahiante <enigaahiante@macs-MacBook-Pro.local>
2019-04-04 07:15:06 +0100

    Initial Commit

---
+++ src/app/layouts/admin-layout/admin-layout.module.ts
@@ -0,0 +1,43 @@
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { ClipboardModule } from 'ngx-clipboard';
+
+import { AdminLayoutRoutes } from './admin-layout.routing';
+import { DashboardComponent } from '../../pages/dashboard/dashboard.component';
+import { IconsComponent } from '../../pages/icons/icons.component';
+import { MapsComponent } from '../../pages/maps/maps.component';
+import { UserProfileComponent } from '../../pages/user-profile/user-profile.component';
+import { TablesComponent } from '../../pages/tables/tables.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { DriversComponent } from 'src/app/pages/drivers/drivers.component';
+import { RequestsComponent } from 'src/app/pages/requests/requests.component';
+import { UsersComponent } from 'src/app/pages/users/users.component';
+import { VehiclesComponent } from 'src/app/pages/vehicles/vehicles.component';
+// import { ToastrModule } from 'ngx-toastr';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forChild(AdminLayoutRoutes),
+    FormsModule,
+    HttpClientModule,
+    NgbModule,
+    ClipboardModule
+  ],
+  declarations: [
+    DashboardComponent,
+    UserProfileComponent,
+    TablesComponent,
+    IconsComponent,
+    MapsComponent,
+    DriversComponent,
+    RequestsComponent,
+    UsersComponent,
+    VehiclesComponent
+  ]
+})
+
+export class AdminLayoutModule {}
---
+++ src/app/layouts/admin-layout/admin-layout.routing.ts
@@ -0,0 +1,23 @@
+import { Routes } from '@angular/router';
+
+import { DashboardComponent } from '../../pages/dashboard/dashboard.component';
+import { IconsComponent } from '../../pages/icons/icons.component';
+import { MapsComponent } from '../../pages/maps/maps.component';
+import { UserProfileComponent } from '../../pages/user-profile/user-profile.component';
+import { TablesComponent } from '../../pages/tables/tables.component';
+import { DriversComponent } from 'src/app/pages/drivers/drivers.component';
+import { RequestsComponent } from 'src/app/pages/requests/requests.component';
+import { UsersComponent } from 'src/app/pages/users/users.component';
+import { VehiclesComponent } from 'src/app/pages/vehicles/vehicles.component';
+
+export const AdminLayoutRoutes: Routes = [
+    { path: 'dashboard',      component: DashboardComponent },
+    { path: 'user-profile',   component: UserProfileComponent },
+    { path: 'tables',         component: TablesComponent },
+    { path: 'icons',          component: IconsComponent },
+    { path: 'maps',           component: MapsComponent },
+    { path: 'drivers',        component: DriversComponent },
+    { path: 'requests',       component: RequestsComponent },
+    { path: 'users',          component: UsersComponent },
+    { path: 'vehicles',       component: VehiclesComponent }
+];
